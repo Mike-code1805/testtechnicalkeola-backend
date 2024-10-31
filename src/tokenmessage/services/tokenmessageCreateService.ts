64381f01cf35692c6afbf76958fb89019ae9cf5b
Mike-code1805 <miguelenrique1805@gmail.com>
2024-10-31 13:51:51 -0500

    update existTokens

--- src/tokenmessage/services/tokenmessageCreateService.ts
+++ src/tokenmessage/services/tokenmessageCreateService.ts
@@ -2,12 +2,19 @@ import { tokenmessageModel } from '../entity/model/tokenmessageModel';
 import { logger } from '../../logger';
 import { MESSAGE_SUCCESS } from '../../constants';
 import { CreateTokenmessage } from '../entity/types/Tokenmessage';
+import { userModel } from '../../user/entity/model/userModel';
 
 export const tokenmessageCreateService = async (body: CreateTokenmessage) => {
   try {
-    await tokenmessageModel.create(body);
+    const existToken = await tokenmessageModel.findOne({ userId: body.userId });
 
-    return MESSAGE_SUCCESS;
+    if (existToken) {
+      await tokenmessageModel.updateOne({ userId: body.userId }, { $set: body });
+      return MESSAGE_SUCCESS;
+    } else {
+      await tokenmessageModel.create(body);
+      return MESSAGE_SUCCESS;
+    }
   } catch (error: any) {
     logger.error('Error validating secret code', {
       instance: 'services',
